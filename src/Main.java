d745b19224f94deeda71c707d9882529a3b98e24
carlosbernard0 <carlosbernardo774@gmail.com>
2023-06-04 00:41:42 -0300

    main e usuario

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Digite o nome: ");
+        String nome = input.nextLine();
+
+        System.out.println("Digite o CPF: ");
+        String cpf = input.nextLine();
+
+        System.out.println("Digite o email: ");
+        String email = input.nextLine();
+
+        System.out.println("Digite a senha: ");
+        String senha = input.nextLine();
+
+        Usuario usuario1 = new Usuario(nome, cpf, email, senha);
+        System.out.println(usuario1);
+
+
     }
 }
---
+++ src/Usuario.java
@@ -0,0 +1,12 @@
+public class Usuario {
+    private String nome, cpf, email, senha;
+
+    public Usuario(String nome, String cpf, String email, String senha) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.email = email;
+        this.senha = senha;
+    }
+
+
+}
